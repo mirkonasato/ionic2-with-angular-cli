3ee1528c756fccfc9337371acef97d7fbd010ad9
Mirko Nasato <mirko@encoded.io>
2016-11-11 23:43:14 +0100

    ion-nav with HomePage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 
 import { AppComponent } from './app.component';
+import { HomePage } from '../pages/home.page';
 
 @NgModule({
   imports: [ IonicModule.forRoot(AppComponent) ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HomePage ],
+  entryComponents: [ HomePage ],
   bootstrap: [ IonicApp ]
 })
 export class AppModule { }
