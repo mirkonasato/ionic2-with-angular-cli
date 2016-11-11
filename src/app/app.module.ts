4ff338a05827eb4e219d2cf2ee4cf4da6ba98b9b
Mirko Nasato <mirko@encoded.io>
2016-11-11 23:36:06 +0100

    import IonicModule and bootstrap IonicApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { IonicApp, IonicModule } from 'ionic-angular';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [ IonicModule.forRoot(AppComponent) ],
+  declarations: [ AppComponent ],
+  bootstrap: [ IonicApp ]
 })
 export class AppModule { }
